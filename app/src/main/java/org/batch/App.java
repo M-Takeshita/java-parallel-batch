58557241cddd7b91071f69e9908c0713c75659ea
M-Takeshita <maskai-takeshita@los-ij.co.jp>
2025-01-12 21:02:00 +0900

    [mod] バッチ用お作法を追加

--- app/src/main/java/org/batch/App.java
+++ app/src/main/java/org/batch/App.java
@@ -1,12 +1,37 @@
 package org.batch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
 
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job sampleJob;
+
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);  // アプリケーションを起動
+        SpringApplication.run(App.class, args);
+    }
+
+    @Autowired
+    public void run() {
+        try {
+            System.out.println("Launching the Job...");
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addLong("time", System.currentTimeMillis()) // 毎回異なるパラメータ
+                    .toJobParameters();
+            jobLauncher.run(sampleJob, jobParameters);
+            System.out.println("Job completed.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/test/java/org/batch/AppTest.java
+++ app/src/test/java/org/batch/AppTest.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", "app should have a greeting");
-    }
+    // @Test public void appHasAGreeting() {
+    //     App classUnderTest = new App();
+    //     assertNotNull("app should have a greeting", "app should have a greeting");
+    // }
 }
